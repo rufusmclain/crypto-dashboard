5d67a920b80c9491d89b13c9b3113139eb556d92
rufusmclain <mshumate3@gmail.com>
2025-07-20 11:45:39 -0700

    Update sw.js
    Add Service Worker

--- sw.js
+++ sw.js
@@ -1,47 +1,18 @@
-const CACHE_NAME = 'crypto-dashboard-v1.0.0';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-// Install event - cache resources
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Fetch event - serve from cache, fallback to network
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Return cached version or fetch from network
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-// Activate event - clean up old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('crypto-dash-v1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html'
+            ]);
         })
-      );
-    })
-  );
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
